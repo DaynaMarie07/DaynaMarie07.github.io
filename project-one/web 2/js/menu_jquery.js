ad5dcc06b466c059b85eb9518413b764cb6de767
dmarie <daynamarie@github.com>
2018-03-24 10:15:22 -0400

    megabytes is the best

--- project-one/web 2/js/menu_jquery.js
+++ project-one/web 2/js/menu_jquery.js
@@ -40,3 +40,108 @@ $(function() {
         }
     });
 });
+var settings = {
+  "async": true,
+  "crossDomain": true,
+  "url": "https://developers.zomato.com/api/v2.1/categories",
+  "method": "GET",
+  "headers": {
+  "user-key": "688e6d4b4b94146be7cd3e7f30f6fef5",
+    
+  }
+}
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
+ 
+ $("button").on("click", function() {
+      console.log("hello");
+  var pizza = $(this).attr("data-food");
+ 
+  $("#gifs-appear-here").empty();
+ 
+  var queryURL= "https://api.giphy.com/v1/gifs/search?q=" +
+   pizza + "&api_key=dc6zaTOxFJmzC&limit=3";
+ 
+ 
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+ 
+  .then(function(response) {
+    var results= response.data;
+    for (var i = 0; i <results.length; i++) {
+    var pizzaDiv = $("<div>");
+    var pizzaImage = $("<img>");
+    pizzaImage.attr("src", results[i].images.fixed_height.url);
+ 
+  
+ 
+    pizzaDiv.append(pizzaImage);
+ 
+ 
+    $("#gifs-appear-here").prepend(pizzaDiv);
+ 
+  }
+ 
+  });
+ 
+ });  
+  
+  
+ var settings = {
+  "async": true,
+  "crossDomain": true,
+  "url": "https://developers.zomato.com/api/v2.1/categories",
+  "method": "GET",
+  "headers": {
+    "user-key": "688e6d4b4b94146be7cd3e7f30f6fef5",
+
+  }
+ }
+ 
+ $.ajax(settings).done(function (response) {
+  console.log(response);
+ });
+//  Message Input
+ 
+//  Message Amir, Deborah Kirchenberg, Na-Nate
+ 
+//  *bold* _italics_ ~strike~ `code` ```preformatted``` >quote
+   
+
+ // Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+$(document).ready(function(){
+  $("#myBtn").click(function(){
+      alert("Hello World");   
+  });
+});
+
+// When the user clicks on the button, open the modal 
+$("#myBtn").on("click", function() {
+  myModal.style.display = "block";
+});
+
+// When the user clicks on <span> (x), close the modal
+$(".close").on("click", function() {
+  myModal.style.display = "none";
+});
+
+// When the user clicks anywhere outside of the modal, close it
+$("#window").on("click", function(event) {
+  if (event.target == modal) {
+      modal.style.display = "none";
+  }
+});
+
